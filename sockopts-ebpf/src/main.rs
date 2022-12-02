670cef7f8893fd39a7848ea8351e0fb3d2a1f7db
John Howard <howardjohn@google.com>
2022-12-02 12:39:24 -0800

    get and set value

--- sockopts-ebpf/src/main.rs
+++ sockopts-ebpf/src/main.rs
@@ -1,24 +1,101 @@
 #![no_std]
 #![no_main]
 
-use aya_bpf::{
-    macros::cgroup_sockopt,
-    programs::SockoptContext,
-};
-use aya_log_ebpf::info;
+use aya_bpf::{bpf_printk, macros::cgroup_sockopt, programs::SockoptContext};
 
-#[cgroup_sockopt(getsockopt,name="sockopts")]
+#[cgroup_sockopt(getsockopt, name = "get_sockopts")]
 pub fn get_sockopts(ctx: SockoptContext) -> i32 {
-
     match try_sockopts(ctx) {
         Ok(ret) => ret,
         Err(ret) => ret,
     }
 }
 
+#[cgroup_sockopt(setsockopt, name = "set_sockopts")]
+pub fn set_sockopts(ctx: SockoptContext) -> i32 {
+    match try_set_sockopts(ctx) {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+#[allow(overflowing_literals)]
+const SOL_CUSTOM: i32 = 0xdeadbeef;
+
 fn try_sockopts(ctx: SockoptContext) -> Result<i32, i32> {
-    info!(&ctx, "getsockopt called");
-    Ok(0)
+    let mut s = unsafe { *ctx.sockopt };
+    if s.level != SOL_CUSTOM {
+        // Not our custom one, send it back
+        return Ok(1);
+    }
+    unsafe {
+        bpf_printk!(b"get SOL_CUSTOM", (*ctx.sockopt).level);
+    }
+    unsafe {
+        if (*ctx.sockopt).__bindgen_anon_2.optval as usize + 128 > (*ctx.sockopt).__bindgen_anon_3.optval_end as usize {
+            return Ok(0);
+        }
+    }
+    // Override kernel error
+    unsafe { (*ctx.sockopt).retval = 0 };
+    unsafe {
+        let data: &mut [u8; 1] = &mut [101; 1];
+        if (*ctx.sockopt).__bindgen_anon_2.optval as usize + 128 > (*ctx.sockopt).__bindgen_anon_3.optval_end as usize {
+            return Ok(0);
+        }
+        // let mut val = 1;
+        // let d = &val as *mut c_void;
+        // *((*ctx.sockopt).__bindgen_anon_2.optval) = *d;
+        // core::ptr::copy_nonoverlapping(data.as_ptr(), ((*ctx.sockopt).__bindgen_anon_2.optval) as *mut u8, 6);
+        // if (*ctx.sockopt).optlen > 3 {
+        //     (*ctx.sockopt).optlen = 3;
+        // }
+        let mut ov = (*ctx.sockopt).__bindgen_anon_2.optval;
+        for v in b"hello" {
+            ov.write_bytes(*v, 1);
+            ov = ov.offset(1)
+        }
+        let mut ol = (*ctx.sockopt).optlen;
+        ol = 1;
+        // ((*ctx.sockopt).__bindgen_anon_2.optval) = data.as_ptr() as *mut c_void;
+        bpf_printk!(b"get sock name %d", (*ctx.sockopt).optname);
+        bpf_printk!(b"get sock len %d", (*ctx.sockopt).optlen);
+        bpf_printk!(b"get sock retval %d", (*ctx.sockopt).retval);
+    }
+    return Ok(1);
+}
+
+fn try_set_sockopts(ctx: SockoptContext) -> Result<i32, i32> {
+    let mut s = unsafe { *ctx.sockopt };
+    if s.level != SOL_CUSTOM {
+        // Not our custom one, send it back
+        return Ok(1);
+    }
+
+    unsafe {
+        bpf_printk!(b"set SOL_CUSTOM %d", (*ctx.sockopt).level);
+    }
+    unsafe {
+        if (*ctx.sockopt).__bindgen_anon_2.optval as usize + 16 > (*ctx.sockopt).__bindgen_anon_3.optval_end as usize {
+            return Ok(0);
+        }
+    }
+    unsafe {
+        bpf_printk!(b"set SOL_CUSTOM %d", (*ctx.sockopt).level);
+
+
+        bpf_printk!(b"set sock name %d", (*ctx.sockopt).optname);
+        bpf_printk!(b"set sock len %d", (*ctx.sockopt).optlen);
+        // bpf_printk!(b"set sock retval %d", (*ctx.sockopt).retval);
+        bpf_printk!(b"set sock optval: %s", (*ctx.sockopt).__bindgen_anon_2.optval);
+    }
+    // Override kernel error
+    // unsafe { (*ctx.sockopt).retval = 0 };
+    // We consumed this value
+    unsafe {
+        (*ctx.sockopt).optlen = -1;
+    }
+    Ok(1)
 }
 
 #[panic_handler]
--- sockopts/src/main.rs
+++ sockopts/src/main.rs
@@ -1,9 +1,16 @@
+use std::mem;
+use std::net::SocketAddr;
+use std::time::Duration;
 use aya::{include_bytes_aligned, Bpf};
 use aya::programs::CgroupSockopt;
 use aya_log::BpfLogger;
 use clap::Parser;
 use log::{info, warn};
-use tokio::signal;
+use tokio::{io, select, signal, time};
+use tokio::net::{TcpListener, TcpStream};
+use std::os::unix::io::AsRawFd;
+use libc::SOL_SOCKET;
+use tokio::task::JoinHandle;
 
 #[derive(Debug, Parser)]
 struct Opt {
@@ -11,7 +18,7 @@ struct Opt {
     cgroup_path: String,
 }
 
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), anyhow::Error> {
     let opt = Opt::parse();
 
@@ -29,17 +36,118 @@ async fn main() -> Result<(), anyhow::Error> {
     let mut bpf = Bpf::load(include_bytes_aligned!(
         "../../target/bpfel-unknown-none/release/sockopts"
     ))?;
-    if let Err(e) = BpfLogger::init(&mut bpf) {
-        // This can happen if you remove all log statements from your eBPF program.
-        warn!("failed to initialize eBPF logger: {}", e);
-    }
-    let program: &mut CgroupSockopt = bpf.program_mut("sockopts").unwrap().try_into()?;
-    let cgroup = std::fs::File::open(opt.cgroup_path)?;
+    // if let Err(e) = BpfLogger::init(&mut bpf) {
+    //     // This can happen if you remove all log statements from your eBPF program.
+    //     warn!("failed to initialize eBPF logger: {}", e);
+    // }
+    let program: &mut CgroupSockopt = bpf.program_mut("get_sockopts").unwrap().try_into()?;
+    let cgroup = std::fs::File::open(opt.cgroup_path.clone())?;
+    program.load()?;
+    program.attach(cgroup)?;
+    let program: &mut CgroupSockopt = bpf.program_mut("set_sockopts").unwrap().try_into()?;
+    let cgroup = std::fs::File::open(opt.cgroup_path.clone())?;
     program.load()?;
     program.attach(cgroup)?;
 
     info!("Waiting for Ctrl-C...");
-    signal::ctrl_c().await?;
+
+    let addr: SocketAddr = "127.0.0.1:1234".parse().unwrap();
+    let client = tokio::spawn(async move {
+        // return;
+        // tokio::time::sleep(Duration::from_millis(250)).await;
+        info!("connecting");
+        let s = TcpStream::connect(addr).await.unwrap();
+        unsafe {
+            let mut optval: [libc::c_uchar; 8] = [0;8];
+            let mut sz = (mem::size_of_val(&optval) * 16) as libc::socklen_t;
+            let ret = libc::getsockopt(
+                s.as_raw_fd(),
+                // libc::SOL_SOCKET,
+                // libc::SO_REUSEPORT,
+                #[allow(overflowing_literals)]
+                0xdeadbeef,
+                12,
+                &mut optval as *const _ as *mut libc::c_void,
+                &mut sz,
+            );
+            if ret != 0 {
+                // tokio::time::sleep(Duration::from_millis(10000000)).await;
+                panic!("{:?}", io::Error::last_os_error());
+            }
+            warn!("got val ret={ret} optval={optval:?}");
+            warn!("string val: {:?}", std::str::from_utf8(&optval));
+        }
+        tokio::time::sleep(Duration::from_secs(1)).await;
+        unsafe {
+            let mut optval: [libc::c_uchar; 11] = b"hello world".map(|b|b as libc::c_uchar);
+            let mut sz = (mem::size_of_val(&optval) * 16) as libc::socklen_t;
+            let ret = libc::setsockopt(
+                s.as_raw_fd(),
+                #[allow(overflowing_literals)]
+                    0xdeadbeef,
+                12,
+                &optval as *const _ as *const libc::c_void,
+                mem::size_of_val(&optval) as libc::socklen_t,
+            );
+            warn!("setsock got val ret={ret} optval={optval:?}");
+            if ret != 0 && ret != -1 {
+                panic!("{:?}", io::Error::last_os_error());
+            }
+        }
+        unsafe {
+            let mut optval: [libc::c_uchar; 8] = [19;8];
+            let mut sz = (mem::size_of_val(&optval) * 16) as libc::socklen_t;
+            let ret = libc::getsockopt(
+                s.as_raw_fd(),
+                #[allow(overflowing_literals)]
+                    0xdeadbeef,
+                12,
+                &mut optval as *const _ as *mut libc::c_void,
+                &mut sz,
+            );
+            if ret != 0 {
+                panic!("{:?}", io::Error::last_os_error());
+            }
+            warn!("got val ret={ret} optval={optval:?}");
+            warn!("string val: {:?}", std::str::from_utf8(&optval));
+        }
+        tokio::time::sleep(Duration::from_secs(1)).await;
+    });
+
+    let server: JoinHandle<()> = tokio::spawn(async move {
+        return;
+        let l = TcpListener::bind(addr).await.unwrap();
+        tokio::time::sleep(Duration::from_secs(3)).await;
+
+        unsafe {
+            let optval: libc::c_int = 1;
+            let ret = libc::setsockopt(
+                l.as_raw_fd(),
+                0x1eadbeef as i32,
+                6,
+                &optval as *const _ as *const libc::c_void,
+                mem::size_of_val(&optval) as libc::socklen_t,
+            );
+            if ret != 0 {
+                panic!("{:?}", io::Error::last_os_error());
+            }
+        }
+
+        l.accept().await.unwrap();
+        tokio::time::sleep(Duration::from_secs(25)).await;
+    });
+    select! {
+        _ = client => {
+            info!("client done");
+        },
+        // _ = server => {
+        //     info!("server done");
+        // },
+        _ = signal::ctrl_c() => {
+
+        }
+    };
+    time::sleep(Duration::from_secs(1));
     info!("Exiting...");
 
     Ok(())
